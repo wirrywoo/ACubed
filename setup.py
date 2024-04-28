5cb3b83c04a3bbdb8365378d83de59e95ea2df50
Wilson Cheung <info@wilsoncheung.me>
2024-04-28 17:12:44 +0100

    removed data directory and updated make commands

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A Redesigned System to Improve',
+    description='A Redesigned System to Improve Skill Measurement in FlashFlashRevolution',
     author='Wilson Cheung',
     license='MIT',
 )
